de41c53b66054c177bc3844a40f75b224e21fa13
Enoch Mwangj <enoch.mwangi@outlook.com>
2022-05-22 14:19:01 +0300

    Add excel document and images

---
+++ ArtWorkPromotion.Seed/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Syncfusion.XlsIO;
+using System;
+
+namespace ArtWorkPromotion.Seed
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string path = "seed data art promotion.xlsx";
+            //Creates a new instance for ExcelEngine
+            ExcelEngine excelEngine = new ExcelEngine();
+
+            //Loads or open an existing workbook through Open method of IWorkbooks
+            IWorkbook workbook = excelEngine.Excel.Workbooks.Open(path);
+        }
+
+    }
+
+}
+
+
